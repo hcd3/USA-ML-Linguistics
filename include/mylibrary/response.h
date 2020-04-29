19b18b19031bcfb7e6988c4bef299454481c82df
hcd3 <hcd3@illinois.edu>
2020-04-29 00:09:11 -0500

    Implement Response object

---
+++ include/mylibrary/response.h
@@ -0,0 +1,29 @@
+//
+// Created by hrishi on 4/28/20.
+//
+
+#ifndef FINALPROJECT_RESPONSE_H
+#define FINALPROJECT_RESPONSE_H
+
+#include <vector>
+#include <string>
+
+namespace mylibrary {
+constexpr size_t kResponseLength = 6;  // Number of questions asked
+const char kChoiceA = 'a';
+const char kChoiceB = 'b';
+const char kChoiceC = 'c';
+
+class Response {
+ public:
+  Response(const std::vector<std::string> &response_as_string, int answer);
+  // Convert response to a vector of characters indicating each choice
+  std::vector<char> choices;
+  // The region each response corresponds to
+  int answer;
+};
+
+
+}  // namespace mylibrary
+
+#endif  // FINALPROJECT_RESPONSE_H
---
+++ src/response.cc
@@ -0,0 +1,15 @@
+//
+// Created by hrishi on 4/28/20.
+//
+
+#include <mylibrary/response.h>
+
+namespace mylibrary {
+Response::Response(const std::vector<std::string> &response_as_string, const int answer) {
+  Response::answer = answer;
+  for (const std::string &choice : response_as_string) {
+    choices.push_back(choice[0]);
+  }
+}
+
+}  // namespace mylibrary
